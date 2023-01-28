f1cd453023a5e605f0060718ceda4f6bc85e8412
Chainz <61576690+Chainz7@users.noreply.github.com>
2023-01-29 00:17:44 +0700

    Login & Signup

--- frontend_react/src/contexts/ContextProvider.jsx
+++ frontend_react/src/contexts/ContextProvider.jsx
@@ -13,7 +13,7 @@ export const ContextProvider = ({children}) => {
         name: 'John',
     })
     // const [token, _setToken] = useState(localStorage.getItem('ACCESS_TOKEN'))
-    const [token, _setToken] = useState(1)
+    const [token, _setToken] = useState(null)
 
     const setToken = (token) => {
         _setToken(token)
